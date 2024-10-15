9790a0523007a41881f04416c596d33297a9b776
Jashwa <jashwa46733@gmail.com>
2024-10-15 17:01:39 +0530

    Update SingleBlog.jsx

--- src/Pages/SingleBlog.jsx
+++ src/Pages/SingleBlog.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import PageIndicator from '../Components/PageIndicator';
-import blogData from '../Components/Data';
-import Faq from '../Components/Faq';
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import PageIndicator from "../Components/PageIndicator";
+import blogData from "../Components/Data";
+import Faq from "../Components/Faq";
 
 const SingleBlog = () => {
   const { id } = useParams();
@@ -15,7 +14,7 @@ const SingleBlog = () => {
 
   return (
     <>
-      <PageIndicator text={blog.title} />
+      <PageIndicator text={'Our Blogs'} />
       <section>
         <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-20">
           <img
@@ -24,12 +23,20 @@ const SingleBlog = () => {
             className="mb-8 h-60 w-full object-cover"
           />
           <h2 className="text-3xl font-bold">{blog.title}</h2>
-          <p className="text-sm text-gray-500">{blog.author} - {blog.date}</p>
-          <p className="mt-8">{blog.content}</p>
+
+           
+            <p className="mb-4 mt-3 text-sm font-semibold bg-[#e3d2bc] py-2 w-32 text-center rounded-md uppercase text-gray-500">
+              {blog.category}
+            </p>
+
+            <p className="text-sm text-gray-500">
+              {blog.author} - {blog.date}
+            </p>
+
+          <p className="mt-8 text-lg">{blog.content}</p>
         </div>
       </section>
-      <Faq/>
-    
+      <Faq />
     </>
   );
 };
